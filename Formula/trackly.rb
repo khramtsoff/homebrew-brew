6b53d6454d6d63d80ec18d56335ffad9b6e6a11c
Vladimir Khramtsov <khramtsov.git@gmail.com>
2023-05-16 15:39:20 +0300

    Remove author property

--- Formula/trackly.rb
+++ Formula/trackly.rb
@@ -1,10 +1,9 @@
 class Trackly < Formula
-  desc "Приложение для удобного просмотра списка пул-реквестов"
+  desc "Приложение для удобного просмотра списка пул-реквестов. Автор: Дмитрий Жаров <DZharov@alfabank.ru>"
   homepage "https://git.moscow.alfaintra.net/projects/IOSUTILS/repos/trackly/browse"
-  url "http://binary/artifactory/repopods/frameworks/Trackly/2.0/Trackly.app.zip"
+  url "http://binary/artifactory/repopods/frameworks/Trackly/#{version}/Trackly.app.zip"
   sha256 "7143eb2b27db1f005a9ed803749d29db2ef9e41ea3d2bda18ea4cd2774325b9f"
   version "2.0"
-  author "Дмитрий Жаров <DZharov@alfabank.ru>"
 
   depends_on macos: ">= :ventura"
 
