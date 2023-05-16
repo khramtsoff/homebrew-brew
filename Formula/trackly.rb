a88004932797dd7bd6157b54b83da0da0de41fd9
Vladimir Khramtsov <khramtsov.git@gmail.com>
2023-05-16 15:43:11 +0300

    More

--- Formula/trackly.rb
+++ Formula/trackly.rb
@@ -1,9 +1,11 @@
-class Trackly < Formula
+cask "trackly" do
+  version "2.0"
+  sha256 "7143eb2b27db1f005a9ed803749d29db2ef9e41ea3d2bda18ea4cd2774325b9f"
+
+  url "http://binary/artifactory/repopods/frameworks/Trackly/#{version}/Trackly.app.zip"
+  name "Trackly"
   desc "Приложение для удобного просмотра списка пул-реквестов. Автор: Дмитрий Жаров <DZharov@alfabank.ru>"
   homepage "https://git.moscow.alfaintra.net/projects/IOSUTILS/repos/trackly/browse"
-  url "http://binary/artifactory/repopods/frameworks/Trackly/#{version}/Trackly.app.zip"
-  sha256 "7143eb2b27db1f005a9ed803749d29db2ef9e41ea3d2bda18ea4cd2774325b9f"
-  version "2.0"
 
   app "Trackly.app"
 end
