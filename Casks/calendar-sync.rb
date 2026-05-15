f011434c6af11ac19557e59a3dde9cd5a0030b10
Vladimir Khramtsov <vkhramtsov@alfabank.ru>
2026-05-15 19:56:46 +0300

    Update calendar-sync to 0.1.0

--- Casks/calendar-sync.rb
+++ Casks/calendar-sync.rb
@@ -1,6 +1,6 @@
 cask "calendar-sync" do
   version "0.1.0"
-  sha256 "f97cb9634eca0da38de6da70f765ffcb4cdac4aed68ef14c52404bc50667faef"
+  sha256 "2abc696e036ee681cb57214a5512c708a1a759c480646d3713b4f384f55cef1e"
 
   url "https://github.com/khramtsoff/eas-calendar-sync-macos/releases/download/v0.1.0/CalendarSync-0.1.0-macos.zip"
   name "CalendarSync"
